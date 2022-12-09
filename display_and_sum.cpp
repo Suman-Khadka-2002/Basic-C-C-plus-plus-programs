86e8211de71db4b1f7a15d0a35195f0d9e2fcf26
Suman Khadka <dnatsu022@gmail.com>
2022-12-09 21:41:43 +0545

    displays and prints the sum

---
+++ display_and_sum.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+
+//  function to display the numbers
+int display(int num){
+    return num;
+}
+
+//  function to add two numbers
+int sum(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
+//  main function
+int main()
+{
+    int a = 10, b = 5;
+    cout<<"Num 1 : "<< display(a)<<endl;
+    cout<<"Num 2 : "<< display(b)<<endl;
+    cout<<"Sum is "<<sum(a, b);
+    return 0;
+}
